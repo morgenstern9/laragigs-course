f4adc3eff7bdbd131fede245209c46b91990ea57
morgenstern9 <smorgenstern@tag.org>
2024-03-07 16:08:32 -0600

    Edit and Delete Listing, User Creation, and User Logout

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,5 +26,23 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 //Store Listing Data
 Route::post('/listings', [ListingController::class, 'store']);
 
+//Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+//Update Listinng Data
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+//Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+//Show Register Create Form
+Route::get('/register', [UserController::class, 'create']);
+
+//Create New User
+Route::post('/users', [UserController::class, 'store']);
+
+//Log User Out
+Route::post('/logout', [UserController::class, 'logout']);
